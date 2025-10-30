5926573fdeae6660cf64dd6d0565e48a96359e6e
stolo-stefan <stefand.stoleriu@gmail.com>
2025-10-30 13:07:42 +0200

    patch: widen pass

--- backend/Entities/User.cs
+++ backend/Entities/User.cs
@@ -21,6 +21,7 @@ public class User
     public string? LastName { get; set; }
 
     [MinLength(8)]
+    [MaxLength(100)]
     public string? AdminPassword { get; set; }
 
     public string Role { get; set; } = "user"; //admin or user
